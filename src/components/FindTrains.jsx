25f0318ca67c47537ba87d74cf4a2b84949fb3de
dhyey2075 <dhyeyhparekh@gmail.com>
2024-06-07 21:37:13 +0530

    search suggestion added

--- src/components/FindTrains.jsx
+++ src/components/FindTrains.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
 
@@ -14,16 +14,77 @@ const App = () => {
   const [route, setRoute] = useState(null)
   const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
   const [show, setShow] = useState(false)
+  const [isTrain, setIsTrain] = useState(false)
+  const [isOpen, setIsOpen] = useState(true)
   const [search, setSearch] = useState("")
+  const [showFrom, setShowFrom] = useState(false)
+  const [showTo, setShowTo] = useState(false)
+  const [clickBtn, setClickBtn] = useState(false)
+  const ref = useRef(null)
 
-    
-  window.onload = function(){
+  window.onload = function () {
     setShow(false)
   }
 
-  const [stations, setStations] = useState([
-    "ST", "ADI", "BRC", "NVS", "RJT", "ANND", "VAPI", "ND", "BH", "BL", "PNU", "AKV", "JAM", "UDN", "BIM", "GIMB", "BHUJ", "VG", "MSH", "DHD", "SUNR", "SIOB", "MAN", "MHD", "BTD", "DWK", "KSB", "MYG", "JND", "KIM", "VRL", "UVD", "UBR", "BVC", "PBR", "GDA", "WKR", "HAPA", "SBIB", "BCOB", "CYI", "KMBL", "UJA", "DHG", "SID", "GDL", "THAN", "JLR", "BKNG", "BLDI", "KSD", "DRL", "PLJ", "AI", "VRR", "MALB", "AML", "WSJ", "AJE", "CLDY", "MLHA", "NUD", "MRL", "DISA", "HVD", "SBT", "RDHP", "LMK", "PDH", "KBRV", "BHTA", "BNVD", "VS", "BJD", "VDA", "SNLR", "LPJ", "DEOR", "ABD", "PAD", "KLL", "NIU", "PAO", "BLD", "PPD", "SMLA", "CVR", "SAU", "GNC", "SJN", "AAR", "SAT", "LTR", "JDH", "MTHP", "RWO", "BAH", "CHP", "SYN", "UA", "ITA", "VRX", "KHDB", "LKZ", "PDF", "DL", "KEMA", "JKA", "GOP", "BMSB", "BIO", "MOL", "PLM", "ANAS", "RUT", "CPN", "GTT", "DJI", "CE", "BHY", "SKR", "BDDR", "PFL", "BJW", "COE", "KRSA", "WAB", "MAM", "RNO", "JDN", "OKHA", "SMNH", "DLJ", "SRGT", "SOJN", "JVN", "VYA", "DQN", "SGD", "SVKD", "LMO", "LM", "RLA", "DAS", "SCH", "BVP", "WC", "BHET", "WTJ", "BIY", "DGI", "MVI", "RUR", "PTN", "CHM", "KNLS", "SOA", "VTJ", "NGA", "ALB", "UJ", "MID", "ALMR", "ACL", "GNST", "JRS", "VDH", "ATUL", "CDA", "LTD", "BJUD", "KDMR", "KSE", "KDI", "GER", "KMLI", "BUBR", "TBV", "NZG", "MDRA", "KSUA", "KTAL", "SDHR", "SDGM", "ADD", "SYQ", "KTNA", "CASA", "VTA", "JUL", "VJK", "SHLV", "VVV", "SNSN", "CBCC", "KDSD", "KVNJ", "BILK", "BKRL", "CHIB"
-  ])
+  useEffect(() => {
+    console.log("changed")
+    if(fromStation==="" || fromStation===null && !clickBtn){
+      setShowFrom(false);
+    }
+    else{
+      console.log("Here")
+      setShowFrom(true);
+    }
+    if(toStation==="" || toStation===null){
+      setShowTo(false);
+    }
+    else{
+      console.log("Here")
+      setShowTo(true);
+    }
+  })
+
+
+  const [stations, setStations] = useState({
+    "New Delhi": "NDLS",
+    "Mumbai Central": "BCT",
+    "Chennai Central": "MAS",
+    "Howrah Junction": "HWH",
+    "Bangalore City Junction": "SBC",
+    "Secunderabad Junction": "SC",
+    "Kolkata Station": "KOAA",
+    "Chhatrapati Shivaji Maharaj Terminus (Mumbai)": "CSMT",
+    "Ahmedabad Junction": "ADI",
+    "Jaipur Junction": "JP",
+    "Hyderabad Deccan Nampally": "HYB",
+    "Pune Junction": "PUNE",
+    "Patna Junction": "PNBE",
+    "Lucknow Charbagh": "LKO",
+    "Kanpur Central": "CNB",
+    "Bhopal Junction": "BPL",
+    "Vadodara Junction": "BRC",
+    "Thiruvananthapuram Central": "TVC",
+    "Guwahati": "GHY",
+    "Nagpur Junction": "NGP",
+    "Surat": "ST",
+    "Bharuch": "BH",
+    "Ankleshwar": "AKV",
+    "Anand": "ANND",
+    "Nadiad": "ND",
+    "Vapi": "VAPI",
+    "Borivali": "BVI",
+    "Rajkot": "RJT",
+    "Bhavnagar": "BVC",
+    "Porbandar": "PBR",
+    "Gandhinagar": "GNC",
+    "Junagadh": "JND",
+    "Dwarka": "DWK",
+    "Bhuj": "BHUJ",
+    "Kutch": "KUTCH",
+    "Mundra": "MND",
+    "Gandhidham": "GIMB",
+    "Jamnagar": "JAM",
+  })
   const navigate = useNavigate()
 
   const handleSearch = async () => {
@@ -44,6 +105,7 @@ const App = () => {
       setError('Error fetching train data.');
     }
     setLoading(false);
+    setIsTrain(true)
   };
 
   function formatDays(days) {
@@ -67,91 +129,164 @@ const App = () => {
     setTr(data)
   }
 
-  const getRoute = (tNo)=>{
+  const getRoute = (tNo) => {
     sessionStorage.setItem("trainNo", tNo)
     navigate("/getroute")
   }
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     console.log(e.target.value)
     setSearch(e.target.value)
+  }
+  const handleFrom = (e) => {
+    console.log(e.target.value);
+    setFromStation(e.target.value);
+    setSearch(e.target.value);
+  };
+  const handleTo = (e) => {
+    console.log(e.target.value);
+    setToStation(e.target.value);
+    setSearch(e.target.value);
+  }
+  const handleSelectFrom = (e) => {
+    console.log(e.target.id);
+    setFromStation(e.target.id);
+    setShowFrom(false);
+    setShowTo(false);
+    setSearch("")
+    setClickBtn(true)
+  };
+  const handleSelectTo = (e) => {
+    console.log(e.target.id);
+    setToStation(e.target.id);
+    setShowFrom(false);
+    setShowTo(false);
+    setSearch("")
+    setClickBtn(true)
   }
 
   return isAuthenticated ? (
     <><div className="App">
-    <h1>Train Schedule</h1>
-    
-    <div className="form">
-      <select name="from" id="from" className="mx-2 p-1" onChange={(e) => setFromStation(e.target.value)}>
-        <option value="">FROM</option>
-        {stations.map((st) => <option key={st} value={st}>{st}</option>)}
-      </select>
-      <select name="to" id="to" className="mx-2 p-1" onChange={(e) => setToStation(e.target.value)}>
-        <option value="">TO</option>
-        {stations.map((st) => <option key={st} value={st}>{st}</option>)}
-      </select>
-      <button onClick={handleSearch}>Search</button>
-    </div>
-    {loading && <><img height="50px" src='../../loader.gif'/><p>Finding trains.....</p></>}
-    {error && <p className="error">{error}</p>}
-    {!loading && trains.length > 0 && (
-      <table>
-        <thead>
-          <tr>
-            <th>Train No</th>
-            <th>Train Name</th>
-            <th>Source</th>
-            <th>Destination</th>
-            <th>Departure</th>
-            <th>Arrival</th>
-            <th>Travel Time</th>
-            <th>Running Days</th>
-          </tr>
-        </thead>
-        <tbody>
-        {trains.filter((name) => name.train_base.train_name.includes(search)).map((train, index) => (
-  <React.Fragment key={train.train_base.train_no}>
-    <tr onClick={() => handleTrainClick(train.train_base.train_no)}>
-      <td>{train.train_base.train_no}</td>
-      <td>{train.train_base.train_name}</td>
-      <td>{train.train_base.from_stn_name}</td>
-      <td>{train.train_base.to_stn_name}</td>
-      <td>{train.train_base.from_time}</td>
-      <td>{train.train_base.to_time}</td>
-      <td>{train.train_base.travel_time}</td>
-      <td>{formatDays(train.train_base.running_days)}</td>
-      <td><button style={{width:"100px"}} className='btn btn-dark' onClick={() => getRoute(train.train_base.train_no)}>Get Route</button></td>
-    </tr>
-    {tr && tr.data && tr.data.train_no === train.train_base.train_no && (
-      <tr>
-        <td colSpan="8">
-          <div className="information-bar">
-            <tr>
-              <td colSpan="8">
-                <div className="information-bar">
-                  <h4>Train Info:</h4>
-                  <td>Train No: {tr.data.train_no}</td>
-                  <td>Train Name: {tr.data.train_name}</td>
-                  <td>From: {tr.data.from_stn_name}</td>
-                  <td>To: {tr.data.to_stn_name}</td>
-                  <td>Type: {tr.data.type}</td>
-                  <td>Distance: {tr.data.distance_from_to}</td>
-                  <td>Average Speed: {tr.data.average_speed}</td>
-                </div>
-              </td>
-            </tr>
-          </div>
-        </td>
-      </tr>
-    )}
-  </React.Fragment>
+      <h1>Train Schedule</h1>
+
+      <div className="form">
+        {
+          isAuthenticated && <><button onClick={() => logout()}>Logout</button><br /></>
+        }
+        {/* <select name="from" id="from" className="mx-2 p-1" open={isOpen} onChange={(e) => setFromStation(e.target.value)}>
+          <option value="">FROM</option>
+          {Object.entries(stations).map(([stationName, stationCode]) => (
+            <option key={stationCode} value={stationCode}>
+              {stationName}
+            </option>
           ))}
-        </tbody>
-      </table>
-      
-    )}
-    <label htmlFor="search Train">Search Train By Name: </label>
-    <input onChange={(e)=>handleChange(e)} type="text" />
-  </div></>
+        </select>
+        <select name="to" id="to" className="mx-2 p-1" onChange={(e) => setToStation(e.target.value)}>
+          <option value="">TO</option>
+          {Object.entries(stations).map(([stationName, stationCode]) => (
+            <option key={stationCode} value={stationCode}>
+              {stationName}
+            </option>
+          ))}
+        </select> */}
+        <label htmlFor="">From:</label>
+        <input
+          style={{borderRadius: "10px", marginLeft: "1rem"}}
+          id="fromInput"
+          className="from"
+          value={fromStation}
+          onChange={handleFrom}
+          type="text"
+        />
+        <div className="list">
+          {showFrom && <ul>
+            {Object.entries(stations).filter(([stationName, stationCode]) => stationName.toLowerCase().includes(search
+            .toLowerCase())).map(([stationName, stationCode]) => (
+              <li onClick={handleSelectFrom} id={stationCode} key={stationCode}>{stationName}{stationCode}</li>
+            ))}
+          
+          </ul>}
+        </div><br />
+        <label htmlFor="">To:</label>
+        <input
+          style={{borderRadius: "10px", marginLeft: "2rem"}}
+          id="toInput"
+          className="to"
+          value={toStation}
+          onChange={handleTo}
+          type="text"
+        />
+        <div className="list">
+          {showTo && <ul>
+            {Object.entries(stations).filter(([stationName, stationCode]) => stationName.toLowerCase().includes(search
+            .toLowerCase())).map(([stationName, stationCode]) => (
+              <li onClick={handleSelectTo} id={stationCode} key={stationCode}>{stationName}{stationCode}</li>
+            ))}
+          
+          </ul>}
+        </div>
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {loading && <><img height="50px" src='/loader.gif' /><p>Finding trains.....</p></>}
+      {error && <p className="error">{error}</p>}
+      {!loading && trains.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Train No</th>
+              <th>Train Name</th>
+              <th>Source</th>
+              <th>Destination</th>
+              <th>Departure</th>
+              <th>Arrival</th>
+              <th>Travel Time</th>
+              <th>Running Days</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trains.filter((name) => name.train_base.train_name.toLowerCase().includes(search.toLowerCase())).map((train, index) => (
+              <React.Fragment key={train.train_base.train_no}>
+                <tr onClick={() => handleTrainClick(train.train_base.train_no)}>
+                  <td>{train.train_base.train_no}</td>
+                  <td>{train.train_base.train_name}</td>
+                  <td>{train.train_base.from_stn_name}</td>
+                  <td>{train.train_base.to_stn_name}</td>
+                  <td>{train.train_base.from_time}</td>
+                  <td>{train.train_base.to_time}</td>
+                  <td>{train.train_base.travel_time}</td>
+                  <td>{formatDays(train.train_base.running_days)}</td>
+                  <td><button style={{ width: "100px" }} className='btn btn-dark' onClick={() => getRoute(train.train_base.train_no)}>Get Route</button></td>
+                </tr>
+                {tr && tr.data && tr.data.train_no === train.train_base.train_no && (
+                  <tr>
+                    <td colSpan="8">
+                      <div className="information-bar">
+                        <tr>
+                          <td colSpan="8">
+                            <div className="information-bar">
+                              <h4>Train Info:</h4>
+                              <td>Train No: {tr.data.train_no}</td>
+                              <td>Train Name: {tr.data.train_name}</td>
+                              <td>From: {tr.data.from_stn_name}</td>
+                              <td>To: {tr.data.to_stn_name}</td>
+                              <td>Type: {tr.data.type}</td>
+                              <td>Distance: {tr.data.distance_from_to}</td>
+                              <td>Average Speed: {tr.data.average_speed}</td>
+                            </div>
+                          </td>
+                        </tr>
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+
+      )}
+      {isTrain && <label htmlFor="search Train">Search Train By Name: </label>}
+      {isTrain && <input ref={ref} onChange={(e) => handleChange(e)} type="text" />}
+    </div></>
 
 
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = (props) => {
 
   return (
     <div>
-    {isLoading && <img src="../data/loader.gif" alt="Loading" />}
+    <div style={{textAlign: 'center'}} className="loader">{isLoading && <img height="50px" style={{textAlign: 'center'}} src="/loader.gif" alt="Loading" />}</div>
     <h1 className='heading'>IRCTC Welcomes You!!</h1>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
@@ -30,7 +30,7 @@ const Home = (props) => {
             <div className='flax'>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               {isAuthenticated && <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/home">Home</a>
+                <a className="nav-link" aria-current="page" href="/">Home</a>
               </li>}
               {isAuthenticated && <li className="nav-item">
                 <a className="nav-link" aria-current="page" href="/pnr">Check PNR Status</a>
@@ -66,7 +66,7 @@ const Home = (props) => {
         <li>Check PNR Status</li>
       </ul>
       {!isAuthenticated && <><h3>Login To avail our service</h3><br /> <br /> <br /></>}
-      <h4>You can login using <img height="30px" src="../../google.png" alt="" />, <img height="30px" src="../../github.png" alt="" /> and <img height="30px" src="../../732221.png" alt="" /></h4>
+      <h4>You can login using <img height="30px" src="/google.png" alt="" />, <img height="30px" src="/github.png" alt="" /> and <img height="30px" src="/public/732221.png" alt="" /></h4>
     </div>
   </div>
   );
